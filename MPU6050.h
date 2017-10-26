57ee68b5778404f3d23ab9b85417c81656a774be
javigonzal <30594085+javigonzal@users.noreply.github.com>
2017-10-26 12:57:02 +0200

    Update MPU6050.h

--- MPU6050.h
+++ MPU6050.h
@@ -128,50 +128,7 @@
 #else
 #define MPU6050_ADDRESS 0x68  // Device address when ADO = 0
 #endif
-enum Ascale {
-	AFS_2G = 0,
-	AFS_4G,
-	AFS_8G,
-	AFS_16G
-};
 
-enum Gscale {
-	GFS_250DPS = 0,
-	GFS_500DPS,
-	GFS_1000DPS,
-	GFS_2000DPS
-};
-
-// Specify sensor full scale
-int Gscale = GFS_250DPS;
-int Ascale = AFS_2G;
-float aRes, gRes; // scale resolutions per LSB for the sensors
-
-				  // Pin definitions
-int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
-
-int16_t accelCount[3];  // Stores the 16-bit signed accelerometer sensor output
-float ax, ay, az;       // Stores the real accel value in g's
-int16_t gyroCount[3];   // Stores the 16-bit signed gyro sensor output
-float gx, gy, gz;       // Stores the real gyro value in degrees per seconds
-float gyroBias[3] = { 0, 0, 0 }, accelBias[3] = { 0, 0, 0 }; // Bias corrections for gyro and accelerometer
-int16_t tempCount;   // Stores the real internal chip temperature in degrees Celsius
-float temperature;
-float SelfTest[6];
-
-uint32_t delt_t = 0; // used to control display output rate
-uint32_t count = 0;  // used to control display output rate
-
-					 // parameters for 6 DoF sensor fusion calculations
-float GyroMeasError = PI * (40.0f / 180.0f);     // gyroscope measurement error in rads/s (start at 60 deg/s), then reduce after ~10 s to 3
-float beta = sqrt(3.0f / 4.0f) * GyroMeasError;  // compute beta
-float GyroMeasDrift = PI * (2.0f / 180.0f);      // gyroscope measurement drift in rad/s/s (start at 0.0 deg/s/s)
-float zeta = sqrt(3.0f / 4.0f) * GyroMeasDrift;  // compute zeta, the other free parameter in the Madgwick scheme usually set to a small or zero value
-float pitch, yaw, roll;
-float deltat = 0.0f;                              // integration interval for both filter schemes
-uint32_t lastUpdate = 0, firstUpdate = 0;         // used to calculate integration interval
-uint32_t Now = 0;                                 // used to calculate integration interval
-float q[4] = { 1.0f, 0.0f, 0.0f, 0.0f };            // vector to hold quaternion
 
 
 class MPU6050 {
@@ -182,6 +139,51 @@ public:
 	//===================================================================================================================
 	//====== Set of useful function to access acceleratio, gyroscope, and temperature data
 	//===================================================================================================================
+	enum Ascale {
+  AFS_2G = 0,
+  AFS_4G,
+  AFS_8G,
+  AFS_16G
+  };
+  
+  enum Gscale {
+    GFS_250DPS = 0,
+    GFS_500DPS,
+    GFS_1000DPS,
+    GFS_2000DPS
+  };
+  
+  // Specify sensor full scale
+  int Gscale = GFS_250DPS;
+  int Ascale = AFS_2G;
+  float aRes, gRes; // scale resolutions per LSB for the sensors
+  
+            // Pin definitions
+  int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+  
+  int16_t accelCount[3];  // Stores the 16-bit signed accelerometer sensor output
+  float ax, ay, az;       // Stores the real accel value in g's
+  int16_t gyroCount[3];   // Stores the 16-bit signed gyro sensor output
+  float gx, gy, gz;       // Stores the real gyro value in degrees per seconds
+  float gyroBias[3] = { 0, 0, 0 }, accelBias[3] = { 0, 0, 0 }; // Bias corrections for gyro and accelerometer
+  int16_t tempCount;   // Stores the real internal chip temperature in degrees Celsius
+  float temperature;
+  float SelfTest[6];
+  
+  uint32_t delt_t = 0; // used to control display output rate
+  uint32_t count = 0;  // used to control display output rate
+  
+             // parameters for 6 DoF sensor fusion calculations
+  float GyroMeasError = PI * (40.0f / 180.0f);     // gyroscope measurement error in rads/s (start at 60 deg/s), then reduce after ~10 s to 3
+  float beta = sqrt(3.0f / 4.0f) * GyroMeasError;  // compute beta
+  float GyroMeasDrift = PI * (2.0f / 180.0f);      // gyroscope measurement drift in rad/s/s (start at 0.0 deg/s/s)
+  float zeta = sqrt(3.0f / 4.0f) * GyroMeasDrift;  // compute zeta, the other free parameter in the Madgwick scheme usually set to a small or zero value
+  float pitch, yaw, roll;
+  float deltat = 0.0f;                              // integration interval for both filter schemes
+  uint32_t lastUpdate = 0, firstUpdate = 0;         // used to calculate integration interval
+  uint32_t Now = 0;                                 // used to calculate integration interval
+  float q[4] = { 1.0f, 0.0f, 0.0f, 0.0f };            // vector to hold quaternion
+  	
 	void getGres() {
 		switch (Gscale)
 		{
