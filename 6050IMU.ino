7a36c99bddcd546b9dd4c63180908667418fb811
javigonzal <30594085+javigonzal@users.noreply.github.com>
2017-10-26 12:55:47 +0200

    Update 6050IMU.ino

--- 6050IMU.ino
+++ 6050IMU.ino
@@ -22,9 +22,9 @@ void setup(void)
   if (c == 0x68) // WHO_AM_I should always be 0x68{  
     Serial.println("MPU6050 is online...");
     
-    myIMU.MPU6050SelfTest(SelfTest); 
-    if(SelfTest[0] < 1.0f && SelfTest[1] < 1.0f && SelfTest[2] < 1.0f && SelfTest[3] < 1.0f && SelfTest[4] < 1.0f && SelfTest[5] < 1.0f) {
-      myIMU.calibrateMPU6050(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
+    myIMU.MPU6050SelfTest(myIMU.SelfTest); 
+    if(myIMU.SelfTest[0] < 1.0f && myIMU.SelfTest[1] < 1.0f && myIMU.SelfTest[2] < 1.0f && myIMU.SelfTest[3] < 1.0f && myIMU.SelfTest[4] < 1.0f && myIMU.SelfTest[5] < 1.0f) {
+      myIMU.calibrateMPU6050(myIMU.gyroBias, myIMU.accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
       myIMU.initMPU6050(); 
       Serial.println("MPU6050 initialized for active data mode...."); // Initialize device for active mode read of acclerometer, gyroscope, and temperature
    }else{
@@ -36,29 +36,29 @@ void setup(void)
 void loop()
 {
   if (myIMU.readByte(MPU6050_ADDRESS, INT_STATUS) & 0x01){
-    myIMU.readAccelData(accelCount);
+    myIMU.readAccelData(myIMU.accelCount);
     myIMU.getAres();
 
-    ax = (float)accelCount[0] * aRes; // - myIMU.accelBias[0];
-    ay = (float)accelCount[1] * aRes; // - myIMU.accelBias[1];
-    az = (float)accelCount[2] * aRes; // - myIMU.accelBias[2];
+   myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
 
-    myIMU.readGyroData(gyroCount);
+    myIMU.readGyroData(myIMU.gyroCount);
     myIMU.getGres();// Read the x/y/z adc values
 
     // Calculate the gyro value into actual degrees per second
     // This depends on scale being set
-    gx = (float)gyroCount[0] * gRes;
-    gy = (float)gyroCount[1] * gRes;
-    gz = (float)gyroCount[2] * gRes;
+    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
+    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
+    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
   }
 
     //  myIMU.updateTime();
-    Now = micros();
-    deltat = ((Now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
-    lastUpdate = Now;
+    myIMU.Now = micros();
+    myIMU.deltat = ((myIMU.Now - myIMU.lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
+    myIMU.lastUpdate = myIMU.Now;
 
-  myIMU.MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f);
+  myIMU.MadgwickQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx*PI/180.0f, myIMU.gy*PI/180.0f, myIMU.gz*PI/180.0f);
   Serial.print("qparent\t");
   Serial.print(q[1], 2);
   Serial.print("\t");
